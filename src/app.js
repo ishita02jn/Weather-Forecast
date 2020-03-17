31984c9d1f416b9386b962fb5d5d1bea2347439f
ishita02jn <ishita02jn@users.github.com>
2020-03-17 19:56:13 +0530

    setup for deployment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const getLatNlong = require('./utils/geocoding')
 const weatherReport = require('./utils/weather')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 //Define paths for express cofig
 const publicDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -83,6 +83,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is started o port number 3000')
+app.listen(port, () => {
+    console.log('Server is started o port number '+port)
 })
